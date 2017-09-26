585c90c3640449362844c958ed111c18be68fc2d
Matt Rothenberg <mattrothenberg@gmail.com>
2017-09-26 09:29:50 -0400

    Build to root dir, update manifest accordingly

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@ const path = require('path')
 module.exports = {
   entry: './index.js',
   output: {
-    filename: 'bundle.js',
-    path: path.resolve('dist')
+    filename: 'quill-image-resize-interact.js',
+    path: path.resolve('.')
   },
   module: {
     rules: [
